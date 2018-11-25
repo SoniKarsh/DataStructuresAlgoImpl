34a52eb20a10f920cdd2dec79ddb831ffd952683
karsh <soni.karsh53@gmail.com>
2018-11-25 17:51:21 +0530

    GCD Implementation

--- GCD.py
+++ GCD.py
@@ -1,21 +1,29 @@
 # Simplest implementation of gcd algorithm.
-def gcd(m, n):
+# GCD Function
+
+
+def gcd(n1, n2):
+    # Factors of m
     fm = []
-    for i in range(1, m + 1):
-        if m % i == 0:
+    for i in range(1, n1 + 1):
+        if n1 % i == 0:
             fm.append(i)
 
+    # Factors of n
     fn = []
-    for j in range(1, n + 1):
-        if n % j == 0:
+    for j in range(1, n2 + 1):
+        if n2 % j == 0:
             fn.append(j)
 
+    # Common factors from fm and fn
     cf = []
     for f in fm:
         if f in fn:
             cf.append(f)
 
+    # Return the rightmost element from the list
     return cf[-1]
+
 
 m = input("Please Enter Number 1 \n")
 n = input("Please Enter Number 2 \n")
