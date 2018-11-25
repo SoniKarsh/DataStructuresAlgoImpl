087c088ae0775bbfe3e76f87538865d5882872fe
karsh <soni.karsh53@gmail.com>
2018-11-25 20:17:31 +0530

    GCD2 Better Implementation

--- GCD1.py
+++ GCD1.py
@@ -5,7 +5,7 @@
 def gcd(n1, n2):
     # Divisor of n1 and n2
     fm = []
-    for i in range(1, min(n1, n2)):
+    for i in range(1, min(n1, n2) + 1):
         if n1 % i == 0 and n2 % i == 0:
             fm.append(i)
 
